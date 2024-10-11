a467a719e22c3118f47646fe17f4bef1397ad1f5
SonderUA <umarbek.sonder@gmail.com>
2024-10-11 19:20:39 +0500

    feat: create functions to play game and round

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
@@ -15,3 +18,37 @@ function getHumanChoice() {
 			return null;
 	}
 }
+
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice === "rock" && computerChoice === "scissors") {
+		console.log("You win! Rock beats Scissors");
+		humanScore++;
+	} else if (humanChoice === "scissors" && computerChoice === "paper") {
+		console.log("You win! Scissors beats Paper");
+		humanScore++;
+	} else if (humanChoice === "paper" && computerChoice === "rock") {
+		console.log("You win! Paper beats Rock");
+		humanScore++;
+	} else if (humanChoice === "scissors" && computerChoice === "rock") {
+		console.log("You lose! Rock beats Scissors");
+		computerScore++;
+	} else if (humanChoice === "paper" && computerChoice === "scissors") {
+		console.log("You lose! Scissors beats Paper");
+		computerScore++;
+	} else if (humanChoice === "rock" && computerChoice === "paper") {
+		console.log("You lose! Paper beats Rock");
+		computerScore++;
+	}
+}
+
+function playGame() {
+	for (let i = 0; i < 5; i++) {
+		let humanChoice = getHumanChoice();
+		let computerChoice = getComputerChoice();
+
+		playRound(humanChoice, computerChoice);
+	}
+	console.log(humanScore > computerScore ? "Human wins!" : "Computer wins!");
+}
+
+// paper wins rock; rock wins scissors; scissors wins paper;
