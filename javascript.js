349961420b2b14cf97f4db121b881cf59d98ae2c
SonderUA <umarbek.sonder@gmail.com>
2024-10-17 15:52:18 +0500

    feat: change javascript to be interactive

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 let humanScore = 0;
 let computerScore = 0;
+let result = document.querySelector("#result");
+let score = document.querySelector("#score");
 
+const buttons = document.querySelectorAll("button");
 const winningConditions = {
 	rock: "scissors",
 	scissors: "paper",
@@ -11,9 +14,7 @@ function getComputerChoice() {
 	return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-	let choice = prompt("Your choice: ").toLowerCase();
-
+function checkHumanChoice(choice) {
 	switch (choice) {
 		case "rock":
 		case "paper":
@@ -27,42 +28,49 @@ function getHumanChoice() {
 
 function showWinner(winner, looser, humanIsWinner = true) {
 	if (winningConditions[winner] === looser) {
-		console.log(
-			`${humanIsWinner ? "You win!" : "You lose!"} ${
-				winner.charAt(0).toUpperCase() + winner.slice(1) // Capitalize winner's choice
-			} beats ${looser}`
-		);
+		result.textContent = `${humanIsWinner ? "You win!" : "You lose!"} ${
+			winner.charAt(0).toUpperCase() + winner.slice(1) // Capitalize winner's choice
+		} beats ${looser}`;
 		humanIsWinner ? humanScore++ : computerScore++;
+	}
+	if (!humanIsWinner) {
+		winner === looser
+			? (result.textContent = "A draw! What a surprise")
+			: null;
 	}
 }
 
 function playRound(humanChoice, computerChoice) {
 	// Check if the user won the round
 	showWinner(humanChoice, computerChoice);
-	// Check if the computer won the round
+	// Check if the computer won the round or draw
 	showWinner(computerChoice, humanChoice, (humanIsWinner = false));
-	if (computerChoice === humanChoice) {
-		console.log("A draw! What a surprise");
-	}
 }
 
-function playGame() {
-	for (let i = 0; i < 5; i++) {
-		let humanChoice;
-		do {
-			humanChoice = getHumanChoice();
-		} while (humanChoice === null);
+function updateScore() {
+	score.textContent = `Score: Player(${humanScore}) and Computer(${computerScore})`;
+}
 
+for (const button of buttons) {
+	button.addEventListener("click", (event) => {
 		let computerChoice = getComputerChoice();
+		let humanChoice = checkHumanChoice(
+			event.target.textContent.toLowerCase()
+		);
 
+		if (humanChoice === null) {
+			console.error("Invalid Choice!");
+		}
 		playRound(humanChoice, computerChoice);
-	}
+		updateScore();
 
-	console.log(
-		humanScore > computerScore
-			? "Human wins!"
-			: humanScore === computerScore
-			? "A draw!"
-			: "Computer wins!"
-	);
+		if (humanScore === 5 || computerScore === 5) {
+			result.style.fontWeight = "bold";
+			if (humanScore > computerScore) {
+				result.textContent = "Human wins!";
+			} else {
+				result.textContent = "Computer wins!";
+			}
+		}
+	});
 }
